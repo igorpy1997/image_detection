40bf80f9842870dfbbd01e6cea5a96159e04e5e3
igorpy1997 <igor.guley@gmail.com>
2024-11-02 22:40:49 +0100

    Sec commit

--- api/api.py
+++ api/api.py
@@ -1,13 +1,12 @@
 import os
 import logging
-
 from dotenv import load_dotenv
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_restful import Api
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required
 
-from Resources.ImageDetection import ImageDetection
+from resources.imageDetection import ImageDetection
 
 load_dotenv()
 app = Flask(__name__, static_folder='static')
@@ -19,9 +18,15 @@ jwt = JWTManager(app)
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+#Mock
 users = {
     "admin": {"password": os.environ.get("ADMIN_PASSWORD")}
 }
+
+
+@app.route('/')
+def client():
+    return render_template('client.html')
 
 
 @app.route('/login', methods=['POST'])
@@ -30,23 +35,19 @@ def login():
     username = data.get("username")
     password = data.get("password")
 
-    logger.info("Login attempt received with username: %s and password: %s", username, password)
-    logger.info("Users database: %s", users)  # Логирование состояния users
-
+    logger.info("Login attempt received with username: %s", username)
     if not username or not password:
         logger.warning("Login attempt with missing username or password")
         return jsonify({"msg": "Bad username or password"}), 401
-
-    logger.info("Attempting to authenticate user: %s", username)
 
     user = users.get(username)
     if user is None or user["password"] != password:
         logger.warning("Login attempt for user: %s failed", username)
         return jsonify({"msg": "Bad username or password"}), 401
 
-    logger.info("User %s logged in successfully", username)
     token = create_access_token(identity=username)
     return jsonify({"token": token})
+
 
 class AuthenticatedImageDetection(ImageDetection):
     decorators = [jwt_required()]
@@ -54,4 +55,4 @@ class AuthenticatedImageDetection(ImageDetection):
 api.add_resource(AuthenticatedImageDetection, '/api/resources/detect')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
